8c6edee2804209e3a786b6933afdb72c6cd71942
George Webster <george@ethyreal.com>
2020-02-11 10:59:05 -0800

    Initial Commit

---
+++ Naruhodo/Naruhodo.h
@@ -0,0 +1,19 @@
+//
+//  Naruhodo.h
+//  Naruhodo
+//
+//  Created by George Webster on 2/11/20.
+//  Copyright © 2020 George Webster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Naruhodo.
+FOUNDATION_EXPORT double NaruhodoVersionNumber;
+
+//! Project version string for Naruhodo.
+FOUNDATION_EXPORT const unsigned char NaruhodoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Naruhodo/PublicHeader.h>
+
+
